4bc676b9be12e0a2d48abf699ac6cc920a88a724
SWASTIK_SHRESTHA <SWASTIK_SHRESTHA@HERO-7>
2018-09-04 21:31:38 +0545

    loginform

---
+++ src/main/java/com/kakchya/kakchyakotha/controller/Login.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.kakchya.kakchyakotha.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author Ujjwal Nakarmi
+ */
+@Controller
+@RequestMapping(value = "/login")
+public class Login {
+    @RequestMapping(method = RequestMethod.GET)
+    public String showHome(){
+               return "login";
+    }
+}
